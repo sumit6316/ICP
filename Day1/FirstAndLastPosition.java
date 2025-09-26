28f65f3d6741d0d11a07e610ad306ab06a56d164
Sumit <2305sumitsahu@gmail.com>
2025-09-26 22:32:45 +0530

    Day 1

---
+++ Day1/FirstAndLastPosition.java
@@ -0,0 +1,36 @@
+package ICP.Day1;
+
+public class FirstAndLastPosition {
+    public int[] searchRange(int[] nums, int target) {
+        return search_Range(nums,target);
+    }
+    public static int[] search_Range(int [] arr,int target){
+        int l=binary_search(arr, target, true);
+        int r=binary_search(arr, target, false);
+        return new int[] {l,r};
+    }
+
+     public static int binary_search(int arr[],int item,boolean first){
+        int ans=-1;
+        int lo=0;
+        int hi=arr.length-1;
+        while(lo<=hi){
+            int mid=lo+(hi-lo)/2;
+            if(arr[mid]==item){
+                ans=mid;
+                if(first){
+                    hi=mid-1;
+                }else{
+                    lo=mid+1;
+                }
+            }else if(item>arr[mid]){
+                lo=mid+1;
+            }else{
+                hi=mid-1;
+            }
+            
+        }
+        return ans;
+
+    }
+}
---
+++ Day1/SlidingWindowMaximum.java
@@ -0,0 +1,28 @@
+package Day1;
+
+import java.util.ArrayDeque;
+import java.util.Deque;
+
+public class SlidingWindowMaximum {
+    public int[] maxSlidingWindow(int[] nums, int k) {
+        int n = nums.length;
+        int res[] = new int[n-k+1];
+        int ri = 0;
+       
+        Deque<Integer> q = new ArrayDeque<>();
+        for(int i=0;i<nums.length;i++){
+            if(!q.isEmpty() && q.peek() == i-k){
+                q.poll();
+            }
+            while(!q.isEmpty() && nums[q.peekLast()]<=nums[i]){
+                q.pollLast();
+            }
+            q.offer(i);
+            if(i>=k-1){
+                res[ri++] = nums[q.peek()];
+            }
+        }
+        return res;
+    }
+    
+}
