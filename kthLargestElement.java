37ed93e8ce0c4555648183aad3f429c46fbffb37
Sumit Sahu <142619166+sumit6316@users.noreply.github.com>
2025-09-27 23:05:01 +0530

    Leetcode 215

---
+++ kthLargestElement.java
@@ -0,0 +1,19 @@
+package Day2;
+
+import java.util.PriorityQueue;
+
+public class kthLargestElement {
+    public int findKthLargest(int[] arr, int k) {
+        PriorityQueue<Integer> pq = new PriorityQueue<>();
+        for(int i=0;i<k;i++){
+            pq.add(arr[i]);
+        }
+        for(int i=k;i<arr.length;i++){
+            if(arr[i]>pq.peek()){
+                pq.poll();
+                pq.add(arr[i]);
+            }
+        }
+        return  pq.peek();
+    }
+}
