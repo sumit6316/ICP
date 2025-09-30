f96f4a67ed4ccc73b433b52691cc768ddeedac69
Sumit Sahu <142619166+sumit6316@users.noreply.github.com>
2025-10-01 00:11:29 +0530

    Add files via upload

---
+++ MergeSortedArrays.java
@@ -0,0 +1,51 @@
+package Algorithms;
+
+public class MergeSortedArrays {
+
+    public static int[] merge_sorted_array(int arr1[],int arr2[]){
+        int n=arr1.length;
+        int m=arr2.length;
+        int []ans=new int[n+m];
+        int i=0,j=0,k=0;
+        while(i<n && j<m){
+            if(arr1[i]<arr2[j]){
+                ans[k]=arr1[i];
+                k++;
+                i++;
+            }else{
+                ans[k]=arr2[j];
+                k++;
+                j++;
+            }
+        }
+        while(i<n){
+            ans[k]=arr1[i];
+                k++;
+                i++;
+        }
+        while(j<m){
+            ans[k]=arr2[j];
+                k++;
+                j++;
+        }
+        return ans;
+    }
+    public static void display(int arr[]){
+        for(int i=0;i<arr.length;i++){
+            System.out.print(arr[i]+" ");
+        }
+    }
+    public static void main(String[] args) {
+        int arr1[]={2,3,5,7,8};
+        int arr2[]={1,3,5,8,9,11,13};
+
+        int a[]=merge_sorted_array(arr1, arr2);
+        display(arr2);
+
+
+
+
+
+
+    }
+}
